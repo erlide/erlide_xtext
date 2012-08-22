9eb417846c5ce2373a97a736a6782e341d71f4f5
Vlad Dumitrescu <vladdu55@gmail.com>
2012-08-22 11:09:28 +0200

    fix line parser problems

--- plugins/org.erlide.builder/src/org/erlide/builder/compiler/DefaultLineParser.xtend
+++ plugins/org.erlide.builder/src/org/erlide/builder/compiler/DefaultLineParser.xtend
@@ -7,14 +7,14 @@ import org.eclipse.core.resources.IMarker
 class DefaultLineParser implements IProblemLineParser {
 	
 	override void parseLine(String line, IFile file, (CompilerProblem)=>void callback) {
-        val List<String> parts = line.split(": ")
-        val List<String> heads = parts.head.split(":") 
-        val warning = parts.size==3
-        val msg = if(warning) parts.get(2) else parts.get(1) 
+        val List<String> parts = line.split(":")
+        val warning = parts.get(2)==" Warning"
+        val msg = parts.tail.tail.join(":").trim
         val severity = if(warning) IMarker::SEVERITY_WARNING else IMarker::SEVERITY_ERROR
-        val nline = Integer::parseInt(heads.tail.head)
+        val nline = Integer::parseInt(parts.tail.head)
         
-        callback.apply(new CompilerProblem(file, msg, nline, severity))                
+        val problem = new CompilerProblem(file, msg, nline, severity)
+        callback.apply(problem)                
     }
 
 }
--- plugins/org.erlide.builder/xtend-gen/org/erlide/builder/compiler/DefaultLineParser.java
+++ plugins/org.erlide.builder/xtend-gen/org/erlide/builder/compiler/DefaultLineParser.java
@@ -1,5 +1,6 @@
 package org.erlide.builder.compiler;
 
+import com.google.common.base.Objects;
 import java.util.List;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IMarker;
@@ -12,31 +13,25 @@ import org.erlide.builder.compiler.IProblemLineParser;
 @SuppressWarnings("all")
 public class DefaultLineParser implements IProblemLineParser {
   public void parseLine(final String line, final IFile file, final Procedure1<? super CompilerProblem> callback) {
-    final List<String> parts = ((List<String>)Conversions.doWrapArray(line.split(": ")));
-    String _head = IterableExtensions.<String>head(parts);
-    final List<String> heads = ((List<String>)Conversions.doWrapArray(_head.split(":")));
-    int _size = parts.size();
-    final boolean warning = (_size == 3);
-    String _xifexpression = null;
+    final List<String> parts = ((List<String>)Conversions.doWrapArray(line.split(":")));
+    String _get = parts.get(2);
+    final boolean warning = Objects.equal(_get, " Warning");
+    Iterable<String> _tail = IterableExtensions.<String>tail(parts);
+    Iterable<String> _tail_1 = IterableExtensions.<String>tail(_tail);
+    String _join = IterableExtensions.join(_tail_1, ":");
+    final String msg = _join.trim();
+    int _xifexpression = (int) 0;
     if (warning) {
-      String _get = parts.get(2);
-      _xifexpression = _get;
+      _xifexpression = IMarker.SEVERITY_WARNING;
     } else {
-      String _get_1 = parts.get(1);
-      _xifexpression = _get_1;
+      _xifexpression = IMarker.SEVERITY_ERROR;
     }
-    final String msg = _xifexpression;
-    int _xifexpression_1 = (int) 0;
-    if (warning) {
-      _xifexpression_1 = IMarker.SEVERITY_WARNING;
-    } else {
-      _xifexpression_1 = IMarker.SEVERITY_ERROR;
-    }
-    final int severity = _xifexpression_1;
-    Iterable<String> _tail = IterableExtensions.<String>tail(heads);
-    String _head_1 = IterableExtensions.<String>head(_tail);
-    final int nline = Integer.parseInt(_head_1);
+    final int severity = _xifexpression;
+    Iterable<String> _tail_2 = IterableExtensions.<String>tail(parts);
+    String _head = IterableExtensions.<String>head(_tail_2);
+    final int nline = Integer.parseInt(_head);
     CompilerProblem _compilerProblem = new CompilerProblem(file, msg, nline, severity);
-    callback.apply(_compilerProblem);
+    final CompilerProblem problem = _compilerProblem;
+    callback.apply(problem);
   }
 }
