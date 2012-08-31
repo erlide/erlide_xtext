3eb38a1e8734399d32b8d5ee60e9144d4863a442
Vlad Dumitrescu <vladdu55@gmail.com>
2012-08-31 11:23:38 +0200

    more warnings cleanup

--- plugins/org.erlide.erlang.tests/xtend-gen/org/erlide/project/buildpath/convert/TestContentProvider.java
+++ plugins/org.erlide.erlang.tests/xtend-gen/org/erlide/project/buildpath/convert/TestContentProvider.java
@@ -10,27 +10,19 @@ import org.erlide.project.buildpath.convert.AbstractContentProvider;
 @SuppressWarnings("all")
 public class TestContentProvider extends AbstractContentProvider {
   public Collection<String> get(final IPath path) {
-    ArrayList<String> _switchResult = null;
+    Collection<String> _switchResult = null;
     String _portableString = path.toPortableString();
     final String _switchValue = _portableString;
     boolean _matched = false;
     if (!_matched) {
       if (Objects.equal(_switchValue,"demo.erlidex")) {
         _matched=true;
-        ArrayList<String> _newArrayList = CollectionLiterals.<String>newArrayList("AAA", "inc1.erlidex", "BBB", "bbb");
+        ArrayList<String> _newArrayList = CollectionLiterals.<String>newArrayList("AAA");
         _switchResult = _newArrayList;
       }
     }
     if (!_matched) {
-      if (Objects.equal(_switchValue,"inc1.erlidex")) {
-        _matched=true;
-        ArrayList<String> _newArrayList_1 = CollectionLiterals.<String>newArrayList("aaa", "bbb");
-        _switchResult = _newArrayList_1;
-      }
-    }
-    if (!_matched) {
-      ArrayList<String> _newArrayList_2 = CollectionLiterals.<String>newArrayList();
-      _switchResult = _newArrayList_2;
+      _switchResult = AbstractContentProvider.NOTHING;
     }
     return _switchResult;
   }
