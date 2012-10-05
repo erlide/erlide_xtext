970a0751e7bf9fba3799602fea52bc0f4b069b25
Vlad Dumitrescu <vladdu55@gmail.com>
2012-10-05 11:53:53 +0200

    AtomValueConverter fix

--- plugins/org.erlide.erlang/src/org/erlide/conversion/AtomValueConverter.java
+++ plugins/org.erlide.erlang/src/org/erlide/conversion/AtomValueConverter.java
@@ -4,6 +4,7 @@ import org.eclipse.xtext.conversion.IValueConverter;
 import org.eclipse.xtext.conversion.ValueConverterException;
 import org.eclipse.xtext.nodemodel.INode;
 
+import com.google.common.base.CharMatcher;
 import com.google.inject.Singleton;
 
 @Singleton
@@ -21,7 +22,8 @@ public class AtomValueConverter implements IValueConverter<String> {
 
     @Override
     public String toString(final String value) throws ValueConverterException {
-        if (value.contains(" ")) {
+        if (value.length() == 0 || value.contains(" ")
+                || CharMatcher.JAVA_UPPER_CASE.apply(value.charAt(0))) {
             return "'" + value + "'";
         }
         return value;
